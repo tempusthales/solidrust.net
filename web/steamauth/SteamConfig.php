fe6a260b95a54f9c290e7c2285bcb436016626a9
Shaun Prince <shaun@techfusion.ca>
2022-04-07 21:47:15 -0700

    Update SteamConfig.php

--- web/steamauth/SteamConfig.php
+++ web/steamauth/SteamConfig.php
@@ -1,6 +1,6 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "40AE3EA263F455A1DBD87A19A1AE0BEF"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['apikey'] = "*****************"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "https://solidrust.net"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "https://solidrust.net"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "https://solidrust.net/discord"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
